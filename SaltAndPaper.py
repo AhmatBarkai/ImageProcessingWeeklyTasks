ac2cfd86ba28aafe7448932ce895e29aac2420c7
AhmatBarkai <100247496+AhmatBarkai@users.noreply.github.com>
2022-11-24 20:45:01 +0300

    Add files via upload

---
+++ SaltAndPaper.py
@@ -0,0 +1,33 @@
+import random
+import cv2
+import matplotlib.pyplot as plt
+
+def addingNoise(image):
+    row, col = image.shape
+    number_of_pixels = random.randint(300, 10000)
+    for i in range(number_of_pixels):
+        y_coord = random.randint(0, row - 1)
+        x_coord = random.randint(0, col - 1)
+        image[y_coord][x_coord] = 255
+    number_of_pixels = random.randint(300, 10000)
+    for i in range(number_of_pixels):
+        y_coord = random.randint(0, row - 1)
+        x_coord = random.randint(0, col - 1)
+        image[y_coord][x_coord] = 0
+
+    return image
+
+
+image = cv2.imread('C://ahmat//images//abc.jpeg', cv2.IMREAD_GRAYSCALE)
+
+cv2.imwrite('C://ahmat//images//img3.jpeg',addingNoise(image))
+
+#  Histogram Displayment
+print(image)
+print(image.ravel())
+hist = cv2.calcHist([image], [0], None, [256], [0, 256])
+plt.hist(image.ravel(), 256, [0, 256])
+plt.title('Histogram of the  paper and salt picture')
+plt.show()
+
+
